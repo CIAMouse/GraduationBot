dcca6677fd6dc256a7e8648b5861ab0dd4045c5d
kaleb418 <kalebwasmuth@gmail.com>
2017-01-15 12:02:14 -0500

    First commit

---
+++ cia-bot.js
@@ -0,0 +1,40 @@
+function MessageHandler(context, event) {
+ 
+    if(event.message == "[Graduation_Date] promysticgamer") {
+       context.sendResponse('>*Trainee Name:* promysticgamer' + '\n\n>*Date Added:* Thu Dec 08, 2016' + '\n\n>*Predicted Graduation:* Thu Dec 29, 2016' + '\n\n>*Extended Date:* Thu Jan 19, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_'); 
+    }
+    if(event.message == "[Graduation_Date] kmsfruit") {
+        context.sendResponse('>*Trainee Name:* kmsfruit' + '\n\n>*Date Added:* Thu Dec 29, 2016' + '\n\n>*Predicted Graduation:* Thu Jan 19, 2017' + '\n\n>*Extended Date:* Thu Feb 9, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_'); 
+    }
+    if(event.message == "[Graduation_Date] nexdrq_yt") {
+        context.sendResponse('>*Trainee Name:* nexdrq_yt' + '\n\n>*Date Added:* Fri Dec 30, 2016' + '\n\n>*Predicted Graduation:* Fri Jan 20, 2017' + '\n\n>*Extended Date:* Fri Feb 10, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+    if(event.message == "[Graduation_Date] xxheyitznaomixx") {
+        context.sendResponse('>*Trainee Name:* xxheyitznaomixx' + '\n\n>*Date Added:* Mon Jan 02, 2017' + '\n\n>*Predicted Graduation:* Mon Jan 23, 2017' + '\n\n>*Extended Date:* Mon Feb 13, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+    if(event.message == "[Graduation_Date] Xxsniper___"){
+        context.sendResponse('>*Trainee Name:* Xxsniper___' + '\n\n>*Date Added:* Mon Jan 02, 2017' + '\n\n>*Predicted Graduation:* Mon Jan 23, 2017' + '\n\n>*Extended Date:* Mon Feb 13, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+    if(event.message == "[Graduation_Date] tbnrmeg") {
+        context.sendResponse('>*Trainee Name:* tbnrmeg' + '\n\n>*Date Added:* Fri Dec 30, 2016' + '\n\n>*Predicted Graduation:* Fri Jan 20, 2017' + '\n\n>*Extended Date:* Fri Feb 10, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+    if(event.message == "[Graduation_Date] mikemnd"){
+        context.sendResponse('>*Trainee Name:* mikemnd' + '\n\n>*Date Added:* Thu Jan 05, 2017' + '\n\n>*Predicted Graduation:* Thu Jan 26, 2017' + '\n\n>*Extended Date:* Thu Feb 16, 2017' + '\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+    if(event.message == "[Graduation_Date] bouncyjeffer"){
+        context.sendResponse('>*Trainee Name:* bouncyjeffer' + '\n\n>*Date Added:* Fri Jan 06, 2017' + '\n\n>*Predicted Graduation:* Fri Jan 27, 2017' + '\n\n>*Extended Date:* Fri Feb 17, 2017' +'\n\n_*The date shown is not final and may change at anytime depending on the situation.*_');
+    }
+}
+
+function EventHandler(context, event) {
+    
+}
+function HttpResponseHandler(context, event) {
+    
+}
+function DbGetHandler(context, event) {
+    
+}
+function DbPutHandler(context, event) {
+    
+}
